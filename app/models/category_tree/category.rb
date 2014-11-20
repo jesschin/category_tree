f32f9eafc43b85436eba135b66a7f456f0d9c1eb
Joan Tiffany Siy <joan@teamcodeflux.com>
2014-11-20 14:47:35 +0800

    Implement search method in Category
    The search method will query the codes and names of
    all the categories based on the given search keys.
    
    This will be used later on for the autocomplete field.
    
    Signed-off-by: Joan Tiffany Siy <joan@teamcodeflux.com>

--- app/models/category_tree/category.rb
+++ app/models/category_tree/category.rb
@@ -4,5 +4,15 @@ module CategoryTree
 
     validates :code, :name, :presence => true
     validates :code, :uniqueness => true
+
+    scope :search, (lambda do |search_keys|
+      if search_keys
+        search_keys.gsub!(' ', '%')
+        where_clause = 'LOWER(CAST(code AS text)) LIKE LOWER(:search) OR ' \
+                       'LOWER(name) LIKE LOWER(:search)'
+        where(where_clause, :search => "%#{search_keys}%")
+      end
+    end)
+
   end
 end
--- spec/models/category_tree/category_spec.rb
+++ spec/models/category_tree/category_spec.rb
@@ -6,5 +6,16 @@ module CategoryTree
     it { should validate_presence_of(:code) }
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:code) }
+
+    it 'should perform a keyword search' do
+      create(:category, :name => 'AAA')
+      create(:category, :name => 'AAB')
+      create(:category, :name => 'ABB')
+      create(:category, :name => 'CCC')
+
+      results = Category.search('AA')
+      expect(results).not_to be_nil
+      expect(results.size).to be_eql 2
+    end
   end
 end
