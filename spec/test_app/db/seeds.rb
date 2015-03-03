b9da246b045276c414d894c37938e5abad926def
Jess Malvin Chin <jess@teamcodeflux.com>
2015-03-03 09:27:39 +0800

    Update the seeds due to non-requirement of icons for the seed data
    Signed-off-by: Jess Malvin Chin <jess@teamcodeflux.com>

--- spec/test_app/db/seeds.rb
+++ spec/test_app/db/seeds.rb
@@ -1,24 +1,16 @@
-fiction = CategoryTree::Category.create(:code => 'FIC',
-                                        :name => 'Fiction')
-prose = fiction.children.create(:code => 'PROSE',
-                                :name => 'Prose')
-fiction.children.create(:code => 'POETRY',
-                        :name => 'Poetry')
+fiction = CategoryTree::Category.new(:code => 'FIC', :name => 'Fiction')
+fiction.save(:validate => false)
+fiction.children.new(:code => 'POETRY', :name => 'Poetry').save(:validate => false)
+prose = fiction.children.new(:code => 'PROSE', :name => 'Prose')
+prose.save(:validate => false)
+prose.children.new(:code => 'YA', :name => 'Young Adult').save(:validate => false)
+fantasy = prose.children.new(:code => 'FAN', :name => 'Fantasy')
+fantasy.save(:validate => false)
+fantasy.children.new(:code => 'MEDFAN', :name => 'Medieval Fantasy').save(:validate => false)
 
-fantasy = prose.children.create(:code => 'FAN',
-                                :name => 'Fantasy')
-fantasy.children.create(:code => 'MEDFAN',
-                        :name => 'Medieval Fantasy')
-
-prose.children.create(:code => 'YA',
-                      :name => 'Young Adult')
-
-nonfiction = CategoryTree::Category.create(:code => 'NFIC',
-                                           :name => 'Non Fiction')
-
-biography = nonfiction.children.create(:code => 'BIO',
-                                       :name => 'Biographies')
-biography.children.create(:code => 'AUTOBIO',
-                          :name => 'Autobiographies')
-biography.children.create(:code => 'POLIBIO',
-                          :name => 'Politician Biographies')
+nonfiction = CategoryTree::Category.new(:code => 'NFIC', :name => 'Non Fiction')
+nonfiction.save(:validate => false)
+biography = nonfiction.children.new(:code => 'BIO', :name => 'Biographies')
+biography.save(:validate => false)
+biography.children.new(:code => 'AUTOBIO', :name => 'Autobiographies').save(:validate => false)
+biography.children.new(:code => 'POLIBIO', :name => 'Politician Biographies').save(:validate => false)
