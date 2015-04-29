c97b1f890dd9c89b12b902867f71d9b283c413e2
Jess Malvin Chin <jmalvinchin@gmail.com>
2015-04-29 08:48:09 +0800

    Remove the api component of the category_tree
    Let the main app handle the serving of apis
    
    Signed-off-by: Jess Malvin Chin <jmalvinchin@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 module CategoryTree
   Engine.routes.draw do
     resources :categories, :as => :categories, :except => [:new, :create]
-
-    namespace :api do
-      resources :categories, :only => [:index, :show]
-    end
   end
 end
